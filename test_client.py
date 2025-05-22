dbb47b1145de7206b38ca1f962f48ee126b3e99d
LBSCallity <callity@limabravo-solutions.de>
2025-05-22 09:06:21 +0200

    ➕ Add test_client.py for local testing

--- test_client.py
+++ test_client.py
@@ -2,7 +2,7 @@ import asyncio
 import websockets
 
 async def send_audio():
-    uri = "ws://localhost:8000/ws/audio"
+    uri = "uri = "wss://callity.onrender.com/ws/audio"
     async with websockets.connect(uri) as ws:
         with open("test_dg.wav", "rb") as f:
             while chunk := f.read(3200):  # ca. 100ms Audio @16kHz
